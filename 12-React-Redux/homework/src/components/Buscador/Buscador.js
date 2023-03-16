8015556c25ec1f4f48ed86dbe5e5b4f14bbb7b75
tomasld13 <tomasld13@gmail.com>
2023-03-16 20:17:10 -0300

    subo cambios m2

--- 12-React-Redux/homework/src/components/Buscador/Buscador.js
+++ 12-React-Redux/homework/src/components/Buscador/Buscador.js
@@ -3,7 +3,6 @@ import { connect } from "react-redux";
 import { Link } from 'react-router-dom';
 import './Buscador.css';
 import {addMovieFavorite, getMovies} from "../../actions/index.js"
-import { get } from "@11ty/eleventy/src/TemplateCache";
 
 
 export class Buscador extends Component {
